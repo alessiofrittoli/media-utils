2f2e134451ada34b279c6a8ea0b36f2c0c984abb
Alessio Frittoli <info@alessiofrittoli.it>
2026-02-16 23:17:40 +0100

    feat: add engine manager

---
+++ src/audio-engine/index.ts
@@ -0,0 +1,25 @@
+import { AudioEngine } from './engine'
+export * from '@/audio-engine/engine'
+
+const audioEngines = new WeakMap<HTMLMediaElement, AudioEngine>()
+
+/**
+ * Delete the `AudioEngine` associated with the given `media` to free-up memory.
+ * 
+ * @param media The HTMLMediaElement.
+ * @returns `true` if the element was successfully removed, or `false` if it was not present.
+ */
+export const destroyEngine = ( media: HTMLMediaElement ) => (
+	audioEngines.delete( media )
+)
+
+
+/**
+ * Get the `AudioEngine` associated with the given `media`.
+ * 
+ * @param media The HTMLMediaElement.
+ * @returns The AudioEngine associated with the given `media` or a new AudioEngine instance if not found.
+ */
+export const getEngine = ( media: HTMLMediaElement ): AudioEngine => (
+	audioEngines.get( media ) || audioEngines.set( media, new AudioEngine( media ) ).get( media )!
+)
